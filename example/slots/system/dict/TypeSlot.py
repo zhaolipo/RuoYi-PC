a3f854b94356e42fd0921904ab45fe01d4d8f663
zhaolipo <guangdujs@163.com>
2024-04-25 23:37:37 +0800

    优化代码

--- example/slots/system/dict/TypeSlot.py
+++
@@ -1,9 +0,0 @@
-from PySide6.QtCore import QObject, Signal, Property, Slot
-from example.api.system.dict.Data import Data
-from example.define import Singleton
-
-@Singleton
-class TypeSlot(QObject):
-
-    def __init__(self):
-        super().__init__()
